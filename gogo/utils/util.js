9a43aadd81fefe239d2047e769d890c1521729e9
wqjj211314 <2744522153@qq.com>
2024-04-08 18:02:10 +0800

    1

--- gogo/utils/util.js
+++ gogo/utils/util.js
@@ -90,7 +90,7 @@ function check_login(app){
     console.log(info["nickName"])
     console.log("比较不出来吗？")
   }
-  if(app.globalData.login_userInfo["user_id"] == ""||app.globalData.login_userInfo["nickName"] == ""||app.globalData.login_userInfo["gender"] == -1||app.globalData.login_userInfo["avatarUrl"] == ""){
+  if(app.globalData.login_userInfo["user_id"] == ""||app.globalData.login_userInfo["user_id"] == undefined||app.globalData.login_userInfo["nickName"] == ""||app.globalData.login_userInfo["gender"] == -1||app.globalData.login_userInfo["avatarUrl"] == ""){
     console.log("跳转到登录界面")
     wx.navigateTo({
       url: '/pages/user/login?userInfo='+encodeURIComponent(JSON.stringify(app.globalData.login_userInfo)),
